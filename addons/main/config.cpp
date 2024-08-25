f3f6df56c84ef5e227756feedb485db283d6aa97
R3vo <robertseibel@outlook.de>
2024-08-25 12:06:24 +0200

    simplified mod settings

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches
     class SpearheadTurretStabilization
     {
         author = "R3vo";
-        name = "Spearhead Turret Stabilization - Main";
+        name = "Spearhead Turret Stabilization";
         requiredAddons[] = {"WW2_SPE_Assets_c_Vehicles_ZZZ_AllVehiclesLoaded_c"};
         requiredVersion = 2.18;
         units[] = {};
